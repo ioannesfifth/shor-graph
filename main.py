5d1c111d474e6889f3f037b5c6fe66ad045cb6af
Ioannes <johnramosver@gmail.com>
2024-11-12 00:52:36 +0800

    added extra graphs

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def plot_fourier(x, y, n, period):
     _plot.ylabel('Amplitude')
     _plot.xticks(np.arange(0, n, period))
 
+    for i_x, i_y in zip(x, y):
+        _plot.text(i_x, i_y, f"({i_x}, {i_y})")
+
     _plot.show()
 
 def main():
@@ -28,6 +31,19 @@ def main():
     plot_fourier(
         coordinates["x"],
         coordinates["y"],
+        n,
+        4
+    )
+    collapsed_coordinates = {"x": [], "y": []}
+    arbritary_y = coordinates["y"][0]
+    for index, y in enumerate(coordinates["y"]):
+        if y == arbritary_y:
+            collapsed_coordinates["x"].append(coordinates["x"][index])
+            collapsed_coordinates["y"].append(coordinates["y"][index])
+
+    plot_fourier(
+        collapsed_coordinates["x"],
+        collapsed_coordinates["y"],
         n,
         4
     )
